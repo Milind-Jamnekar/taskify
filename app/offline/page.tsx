4abe435911415a567b064c1d4e1f04ef698404cd
Milind Jamnekar <milindjamnekar502@gmail.com>
2024-03-19 20:35:22 +0530

    pwa added

---
+++ app/offline/page.tsx
@@ -0,0 +1,18 @@
+import prisma from "@/prisma/db";
+import TaskList from "../_components/TaskList";
+
+export default async function Home() {
+  let tasks = await prisma.tasks.findMany();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div>
+        <h1 className="text-center text-6xl pb-14">Taskify App</h1>
+      </div>
+
+      <div className=" md:max-w-xl p-6">
+        <TaskList tasks={tasks} />
+      </div>
+    </main>
+  );
+}
